aa159aaed4f38f777b4eb586b73127b89428d77f
Mikhail <rusmih98@yandex.ru>
2020-03-16 16:18:42 +0700

    кнопки

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -31,6 +31,10 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label24 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.Column3 = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewComboBoxColumn();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new WindowsFormsApp1.CalendarColumn();
@@ -83,17 +87,15 @@
             this.label8 = new System.Windows.Forms.Label();
             this.textBox8 = new System.Windows.Forms.TextBox();
             this.label9 = new System.Windows.Forms.Label();
-            this.dataGridView2 = new System.Windows.Forms.DataGridView();
-            this.label24 = new System.Windows.Forms.Label();
-            this.Column3 = new System.Windows.Forms.DataGridViewComboBoxColumn();
-            this.Column4 = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize) (this.dataGridView2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize) (this.dataGridView1)).BeginInit();
             this.panel3.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize) (this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -129,6 +131,35 @@
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Общие сведения";
             this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label24
+            // 
+            this.label24.Location = new System.Drawing.Point(493, 353);
+            this.label24.Name = "label24";
+            this.label24.Size = new System.Drawing.Size(139, 18);
+            this.label24.TabIndex = 20;
+            this.label24.Text = "Знания языков";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode =
+                System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[]
+                {this.Column3, this.Column4});
+            this.dataGridView2.Location = new System.Drawing.Point(493, 375);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(535, 150);
+            this.dataGridView2.TabIndex = 19;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Язык";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Степень знания";
+            this.Column4.Name = "Column4";
             // 
             // dataGridView1
             // 
@@ -554,40 +585,31 @@
             this.label9.TabIndex = 17;
             this.label9.Text = "Дополнительный телефон";
             // 
-            // dataGridView2
+            // button1
             // 
-            this.dataGridView2.ColumnHeadersHeightSizeMode =
-                System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[]
-                {this.Column3, this.Column4});
-            this.dataGridView2.Location = new System.Drawing.Point(493, 375);
-            this.dataGridView2.Name = "dataGridView2";
-            this.dataGridView2.Size = new System.Drawing.Size(535, 150);
-            this.dataGridView2.TabIndex = 19;
+            this.button1.Location = new System.Drawing.Point(12, 464);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 23);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Сохранить изменения";
+            this.button1.UseVisualStyleBackColor = true;
             // 
-            // label24
+            // button2
             // 
-            this.label24.Location = new System.Drawing.Point(493, 353);
-            this.label24.Name = "label24";
-            this.label24.Size = new System.Drawing.Size(139, 18);
-            this.label24.TabIndex = 20;
-            this.label24.Text = "Знания языков";
-            // 
-            // Column3
-            // 
-            this.Column3.HeaderText = "Язык";
-            this.Column3.Name = "Column3";
-            // 
-            // Column4
-            // 
-            this.Column4.HeaderText = "Степень знания";
-            this.Column4.Name = "Column4";
+            this.button2.Location = new System.Drawing.Point(12, 493);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 23);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "Отменить изменения";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1344, 726);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox8);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.textBox7);
@@ -612,12 +634,12 @@
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize) (this.dataGridView2)).EndInit();
             ((System.ComponentModel.ISupportInitialize) (this.dataGridView1)).EndInit();
             this.panel3.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize) (this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
         }
@@ -683,5 +705,7 @@
         private System.Windows.Forms.Label label24;
         private System.Windows.Forms.DataGridViewComboBoxColumn Column4;
         private System.Windows.Forms.DataGridViewComboBoxColumn Column3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
