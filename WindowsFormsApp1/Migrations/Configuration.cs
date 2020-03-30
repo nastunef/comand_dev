82ee8559edc460becca6f52382940e24d4392145
kamotora <kamotora@yandex.ru>
2020-03-31 02:08:39 +0700

    Поправлено понемногу, работает добавление работников, не работает добавление мест в таблицу

---
+++ WindowsFormsApp1/Migrations/Configuration.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+
+namespace WindowsFormsApp1.Migrations
+{
+    internal sealed class Configuration : DbMigrationsConfiguration<WindowsFormsApp1.Model1>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+    } 
+}
