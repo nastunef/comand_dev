c7327cbbe69dd57c988689010b721a510c488a1c
kamotora <kamotora@yandex.ru>
2020-04-22 16:20:34 +0700

    Предыдущий коммит чужой код изменил

--- WindowsFormsApp1/AddPlaceKomandForm.Designer.cs
+++ WindowsFormsApp1/AddPlaceKomandForm.Designer.cs
@@ -46,48 +46,48 @@ namespace Komandirovki
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(15, 14);
+            this.label1.Location = new System.Drawing.Point(17, 16);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(45, 14);
+            this.label1.Size = new System.Drawing.Size(52, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "Страна:";
             // 
             // CountryComboBox
             // 
             this.CountryComboBox.FormattingEnabled = true;
-            this.CountryComboBox.Location = new System.Drawing.Point(120, 11);
+            this.CountryComboBox.Location = new System.Drawing.Point(140, 13);
             this.CountryComboBox.Name = "CountryComboBox";
-            this.CountryComboBox.Size = new System.Drawing.Size(187, 21);
+            this.CountryComboBox.Size = new System.Drawing.Size(217, 23);
             this.CountryComboBox.TabIndex = 1;
             // 
             // label2
             // 
-            this.label2.Location = new System.Drawing.Point(15, 48);
+            this.label2.Location = new System.Drawing.Point(17, 55);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(45, 14);
+            this.label2.Size = new System.Drawing.Size(52, 16);
             this.label2.TabIndex = 2;
             this.label2.Text = "Город:";
             // 
             // label3
             // 
-            this.label3.Location = new System.Drawing.Point(15, 84);
+            this.label3.Location = new System.Drawing.Point(17, 97);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(76, 14);
+            this.label3.Size = new System.Drawing.Size(89, 16);
             this.label3.TabIndex = 4;
             this.label3.Text = "Организация";
             // 
             // OrganizationTextBox
             // 
-            this.OrganizationTextBox.Location = new System.Drawing.Point(120, 81);
+            this.OrganizationTextBox.Location = new System.Drawing.Point(140, 93);
             this.OrganizationTextBox.Name = "OrganizationTextBox";
-            this.OrganizationTextBox.Size = new System.Drawing.Size(187, 20);
+            this.OrganizationTextBox.Size = new System.Drawing.Size(217, 23);
             this.OrganizationTextBox.TabIndex = 5;
             // 
             // SaveButton
             // 
-            this.SaveButton.Location = new System.Drawing.Point(21, 111);
+            this.SaveButton.Location = new System.Drawing.Point(24, 128);
             this.SaveButton.Name = "SaveButton";
-            this.SaveButton.Size = new System.Drawing.Size(79, 22);
+            this.SaveButton.Size = new System.Drawing.Size(92, 25);
             this.SaveButton.TabIndex = 6;
             this.SaveButton.Text = "Сохранить";
             this.SaveButton.UseVisualStyleBackColor = true;
@@ -95,9 +95,9 @@ namespace Komandirovki
             // 
             // ExitButton
             // 
-            this.ExitButton.Location = new System.Drawing.Point(148, 111);
+            this.ExitButton.Location = new System.Drawing.Point(173, 128);
             this.ExitButton.Name = "ExitButton";
-            this.ExitButton.Size = new System.Drawing.Size(159, 22);
+            this.ExitButton.Size = new System.Drawing.Size(185, 25);
             this.ExitButton.TabIndex = 7;
             this.ExitButton.Text = "Выйти без сохранения";
             this.ExitButton.UseVisualStyleBackColor = true;
@@ -105,9 +105,9 @@ namespace Komandirovki
             // 
             // CityTextBox
             // 
-            this.CityTextBox.Location = new System.Drawing.Point(120, 45);
+            this.CityTextBox.Location = new System.Drawing.Point(140, 52);
             this.CityTextBox.Name = "CityTextBox";
-            this.CityTextBox.Size = new System.Drawing.Size(187, 20);
+            this.CityTextBox.Size = new System.Drawing.Size(217, 23);
             this.CityTextBox.TabIndex = 8;
             // 
             // pLACETRIPBindingSource
@@ -116,9 +116,9 @@ namespace Komandirovki
             // 
             // AddPlaceKomandForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(322, 140);
+            this.ClientSize = new System.Drawing.Size(376, 162);
             this.Controls.Add(this.CityTextBox);
             this.Controls.Add(this.ExitButton);
             this.Controls.Add(this.SaveButton);
@@ -128,24 +128,23 @@ namespace Komandirovki
             this.Controls.Add(this.CountryComboBox);
             this.Controls.Add(this.label1);
             this.Name = "AddPlaceKomandForm";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Добавить место назначения";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.AddPlaceKomandForm_FormClosing);
             ((System.ComponentModel.ISupportInitialize) (this.pLACETRIPBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
         }
 
-        private System.Windows.Forms.TextBox CityTextBox;
-        private System.Windows.Forms.ComboBox CountryComboBox;
-        private System.Windows.Forms.Button ExitButton;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox OrganizationTextBox;
-        private System.Windows.Forms.BindingSource pLACETRIPBindingSource;
-        private System.Windows.Forms.Button SaveButton;
-
         #endregion
+        private System.Windows.Forms.TextBox OrganizationTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox CountryComboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.TextBox CityTextBox;
+        private System.Windows.Forms.BindingSource pLACETRIPBindingSource;
     }
 }
