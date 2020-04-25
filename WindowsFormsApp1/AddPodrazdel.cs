b2f38f0f62f2309fafeebc530468e1247fad84aa
Th3Ch3shir3Cat <gerbert98@mail.ru>
2020-04-25 22:05:15 +0300

    Добавление подразделения

---
+++ WindowsFormsApp1/AddPodrazdel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+
+namespace WindowsFormsApp1
+{
+    public partial class AddPodrazdel : Form
+    {
+        private  Model1 model = new Model1();
+        public AddPodrazdel()
+        {
+            InitializeComponent();
+        }
+
+        private void AddPodrazdel_Load(object sender, EventArgs e)
+        {
+            IQueryable<PODRAZDELORG> podrazdelorgs = model.PODRAZDELORG;
+            foreach (PODRAZDELORG podrazdelorg in podrazdelorgs)
+            {
+                mainPodrazdel.Items.Add(podrazdelorg);
+            }
+        }
+    }
+}
